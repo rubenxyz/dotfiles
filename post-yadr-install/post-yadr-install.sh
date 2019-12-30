ebaa5df0760c5d0436dfe8cf09402fbacfa715e1
Ruben Sznajderman <log@kamiko.xyz>
2019-12-30 09:15:24 +0100

    Update post-yadr-install.sh

--- post-yadr-install/post-yadr-install.sh
+++ post-yadr-install/post-yadr-install.sh
@@ -18,16 +18,12 @@ yadr vim-add-plugi -u https://github.com/mcchrish/fountain.vim
 #Goyo, the undistracted writing plugin
 yadr vim-add-plugin -u https://github.com/junegunn/goyo.vim
 
-#Swiftly comment and uncomment lines
-yadr vim-add-plugin -u https://github.com/manasthakur/vim-commentor
-
 ######################################
 #       Append alias list            #
 ######################################
 wget https://raw.githubusercontent.com/rubensz/dotfiles/master/post-yadr-install/ruben-alias.txt -O ~/.ruben/ruben-alias.txt 
 cat ~/.ruben/ruben-alias.txt  >> ~/.yadr/zsh/aliases.zsh
 rm ~/.ruben/ruben-alias.txt
-
 
 ############################
 
